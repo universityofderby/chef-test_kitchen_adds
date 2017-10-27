c2146ad94946967b77618ecae0126978942a8956
Richard Lock <r.j.lock@derby.ac.uk>
2017-10-27 11:56:58 +0100

    Added changelog and updated version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'its-chef@derby.ac.uk'
 license 'Apache-2.0'
 description 'The test_kitchen_adds cookbook provides Active Directory domain controller and member helper recipes in test kitchen.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 source_url 'https://github.com/universityofderby/chef-test_kitchen_adds'
 issues_url 'https://github.com/universityofderby/chef-test_kitchen_adds/issues'
 chef_version '>= 12.0'
